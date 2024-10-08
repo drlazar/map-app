aedec7fb006cadab6d836339b94a495b0d036bc3
drlazar <49284596+drlazar@users.noreply.github.com>
2024-10-08 21:53:45 +0000

    update template to include latitude and longitude code and dependencies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(layout="wide")
+
+st.write("""
+# Map Coordinates Application
+"""
 )
+
+st.header("Please enter your desired coordinates below")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    latitude = st.number_input("Latitude:", value = 39.32889, format="%.5f")
+with col2:  
+    longitude = st.number_input("Longitude:", value = -76.62028, format="%.5f")
+
+coord = pd.DataFrame({
+                        "latitude" : [latitude], 
+                        "longitude" : [longitude]
+                    }
+                    )
+
+st.map(coord, latitude="latitude", longitude="longitude", zoom = 13)
